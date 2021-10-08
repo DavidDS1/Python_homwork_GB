1c38ad3964c19d7e658ef469aff1f9ad3d58df19
davidaskerov <askerovdf23@mail.ru>
2021-10-08 18:20:01 +0300

    Homwork 1 added

---
+++ homwork1/task_3.py
@@ -0,0 +1,7 @@
+#задание 3
+users_number = input('Введите число n: ')
+a = int(users_number)
+b= int(users_number*2)
+c = int(users_number*3)
+sum = a+b+c
+print(f'сумма выражения n+nn+nn равна {sum}')
