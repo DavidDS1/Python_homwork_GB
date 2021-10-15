6d6c254d184bcd37da6dce987856d86915093ff8
davidaskerov <askerovdf23@mail.ru>
2021-10-15 23:18:23 +0300

    Homwork 2 added

---
+++ homwork_2/task_3.py
@@ -0,0 +1,21 @@
+#задание 3
+print('Решение через словарь')
+month_1=(input('Введите № месяца: '))
+
+season_1 = {'1': 'winter', '2': 'winter', '3': 'spring',
+            '4':'spring ', '5': 'spring ', '6': 'summer ',
+            '7':'summer', '8':'summer', '9':'autumn',
+            '10':'autumn', '11':'autumn', '12':'winter'}
+result_1 = season_1.get(month_1)
+print(f'Сейчас время года - {result_1}')
+
+print('Решение через списки')
+month_2= int(input('Введите № месяца: '))
+season_2 = ['winter', 'winter', 'winter',
+            'spring', 'spring', 'spring',
+            'summer', 'summer', 'summer',
+            'autumn', 'autumn', 'autumn']
+result_2 = season_2[month_2+1]
+print(f'Сейчас время года - {result_2}')
+
+
