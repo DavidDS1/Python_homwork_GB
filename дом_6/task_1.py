94cf4329e41dc10c8f1902b563b0acaa6dfdf07c
DavidDS1 <91973929+DavidDS1@users.noreply.github.com>
2021-11-03 23:14:29 +0800

    Update дом_6/task_1.py

--- дом_6/task_1.py
+++ дом_6/task_1.py
@@ -11,19 +11,3 @@ class TrafficLight:
                 time.sleep(result_dict.setdefault(k))
             j += 1
         return ' '
-
-
-trfl = TrafficLight()
-print(trfl.running())
-
-#задание 2
-class Road:
-    def __init__(self,_length, _width):
-        self._length=_length
-        self._width=_width
-    def weight (self):
-        result_weight =self._length * self._length*25*5
-        weight_ton = result_weight/1000
-        return print(f'{weight_ton} тонн')
-materials= Road(1000, 10)
-materials.weight()
